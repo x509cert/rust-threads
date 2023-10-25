424f0914f61e50718b312528ad698c6f557f6ca8
Michael Howard <mikehow@microsoft.com>
2023-10-24 22:20:19 -0500

    Minor edits

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::thread;
 
 fn main() {
-    let handles: Vec<_> = (0..20).map(|_| {
+    let handles: Vec<_> = (0..10).map(|_| {
         thread::spawn(|| {
             let current_thread = thread::current();
             let thread_id = current_thread.id();
-            println!("Thread with ID: {:?}", thread_id);
+            println!("{:?}", thread_id);
 
             let mut x = 0;
-            for _ in 0..2_140_000_000 {
+            for _ in 0..2_140_000 {
                 x += 1
             }
             x
